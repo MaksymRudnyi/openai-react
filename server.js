b415658ea58cec8fc2ba452fd02eb3d96ad4593c
Maksym Rudnyi <max.prog.zz@gmail.com>
2024-04-11 18:21:37 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import cors from 'cors';
+import OpenAI from 'openai';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+console.log('API_KEY:', process.env.API_KEY);
+
+const openai = new OpenAI({
+  apiKey: process.env['API_KEY'], // This is the default and can be omitted
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/message', async (req, res) => {
+  try {
+    console.log('Received request:', req.body);
+
+    // const stream = openai.beta.chat.completions.stream({
+    //
+    // });
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: 'user', content: req.body.message }],
+      model: 'gpt-4-turbo',
+    });
+
+    res.status(200).send({ status: 'Message received', received: chatCompletion.choices[0].message.content });
+    res.end();
+  } catch (e) {
+    res.write('An error occurred');
+    res.end();
+    console.error(e);
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react';
+import Markdown from 'react-markdown'
+
+const makeRequestToChatGPT = async (message) => {
+  const response = await fetch('http://localhost:3000/message', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ message }),
+  });
+
+  return response.json();
+}
+
+function App() {
+  const [chatHistory, setChatHistory] = useState([]); // [{ message: 'Hello', response: 'Hi' }
+  const [currentMessage, setCurrentMessage] = useState('');
+
+  const askChatGPT = async (e) => {
+    e.preventDefault();
+    setChatHistory((state) => ([...state, {
+      text: currentMessage,
+      sender: 'You'
+    }]))
+
+    const response = await makeRequestToChatGPT(currentMessage);
+    setCurrentMessage('');
+
+    setChatHistory((state) => ([...state, {
+      text: response.received,
+      sender: 'ChatGPT'
+    }]))
+  }
+
+  console.log('chatHistory', chatHistory);
+
+  return (
+    <div className="flex flex-col h-full p-4 absolute w-full">
+      <div className="overflow-auto pb-16 h-full ">
+        {chatHistory.map((message, index) => (
+          <div key={message.text}>
+            <p>{message.sender}</p>
+            <div key={index} className="p-2 mb-2 border rounded">
+              <Markdown>{message.text}</Markdown>
+            </div>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={askChatGPT} className="flex h-16">
+        <input
+          type="text"
+          value={currentMessage}
+          onChange={(e) => setCurrentMessage(e.target.value)}
+          className="flex-grow mr-2 p-2 border rounded"
+        />
+        <button type="submit" className="p-2 bg-blue-500 text-white rounded">
+          Send
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
